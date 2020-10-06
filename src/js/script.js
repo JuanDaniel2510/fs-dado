061de2f37519d29bc7d0f3f0b67932bba25ee534
JuanDaniel2510 <juan_daniel_2510@hotmail.com>
2020-10-06 19:55:25 +0100

    ported fomr codepen.io

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+let dots = new Array(6);
+for (var i = 0; i < 7; i++)
+  dots[i] = document.querySelector('#point' + (i + 1));
+
+
+function launch() {
+  var num = getRandomFace();
+  console.log(num);
+  switch (num) {
+    case 1:
+      show([4]);
+      break;
+    case 2:
+      show([1, 7]);
+      break;
+    case 3:
+      show([1, 4, 7]);
+      break;
+    case 4:
+      show([1, 2, 6, 7]);
+      break;
+    case 5:
+      show([1, 2, 4, 6, 7]);
+      break;
+    case 6:
+      show([1, 2, 3, 5, 6, 7]);
+      break;
+  }
+}
+
+function getRandomFace() {
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+function show(points) {
+  for (var i = 0; i < dots.length; i++)
+    dots[i].classList.add('hide');
+
+  for (var j = 0; j < points.length; j++)
+    dots[points[j] - 1].classList.remove('hide');
+}
+
+document.getElementById("push").addEventListener("click",launch);
